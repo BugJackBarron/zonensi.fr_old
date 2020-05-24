0266860ab702b158f336f1e0f5eb2db3bb8b87f0
BugJackBarron <rincevent76@gmail.com>
2020-05-24 11:16:35 +0200

    Finalisation squelette + ajout bdd

--- app.py
+++ app.py
@@ -1,12 +1,48 @@
-from flask import Flask
+from flask import Flask,render_template,url_for,request,session,redirect,flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from flask_login import LoginManager,UserMixin,login_user,login_required,current_user,logout_user
+from wtforms import StringField, PasswordField, SubmitField, SelectField,widgets, HiddenField
+from wtforms.validators import DataRequired, length, InputRequired, EqualTo
+from flask_admin import Admin, AdminIndexView
+from flask_admin.contrib.sqla import ModelView
+import datetime
+
+from werkzeug import security
 
 app = Flask(__name__)
+app.config['secret_key'] = 'secret'
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///static/zonensidb.sqlite3'
+
+###________CONFIGURATION DES MODULES_____________###
+db=SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+###_________________CLASSES DES TABLES POUR SQLALCHEMY________________________________###
+class User(UserMixin, db.Model) :
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(40), unique=True, nullable=False)
+    password = db.Column(db.String(40), nullable=False)
+
+
+
+class BlogPost(db.Model) :
+    id_post = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(40), unique=True)
+    course = db.Column(db.String(40))
+    category = db.Column(db.String(40))
+    subcategory = db.Column(db.String(40))
+    content = db.Column(db.Text)
+
+
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('squelette.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
