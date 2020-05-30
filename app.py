d12b0539f5fe45beff98f440a5e74ec14400a767
BugJackBarron <rincevent76@gmail.com>
2020-05-30 11:29:19 +0200

    Ajouty interface admin

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///static/zonensidb.sqlite3'
 
 ###________CONFIGURATION DES MODULES_____________###
 db=SQLAlchemy(app)
-#login_manager = LoginManager()
-#login_manager.init_app(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 Markdown(app)
 
 ###_________________CLASSES DES TABLES POUR SQLALCHEMY________________________________###
@@ -28,7 +28,6 @@ class User(UserMixin, db.Model) :
     id = db.Column(db.Integer, primary_key=True)
     login = db.Column(db.String(40), unique=True, nullable=False)
     password = db.Column(db.String(40), nullable=False)
-
 
 
 class BlogPost(db.Model) :
@@ -40,8 +39,20 @@ class BlogPost(db.Model) :
     content = db.Column(db.Text)
     date=db.Column(db.DateTime)
 
+
+class Categories(db.Model) :
+    little_name = db.Column(db.String(10))
+    real_name = db.Column(db.String(40))
+    parent = db.Column(db.Integer)
+    idg = db.Column(db.Integer,primary_key=True)
+    idd = db.Column(db.Integer,primary_key=True)
+
+
 ###________________FORMULAIRES WTFORMS______________________________###
 
+class LoginForm(FlaskForm):
+    login = StringField('Login', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
 
 class PostForm(FlaskForm):
     title = StringField('Titre', validators=[DataRequired()])
@@ -51,39 +62,97 @@ class PostForm(FlaskForm):
                                                         ('esci', 'Ens. Sci.'),
                                                         ('misc', 'Misc')],
                         default = 'maths')
+    #category = SelectField('category', choices=[])
+    #subcategory = SelectField('chapter', choices=[('new','New')])
     content = TextAreaField("Contenu")
+
+
+###____________________MODIFICATIONS DES VUES DE L'ADMIN POUR Flask-Admin_______________________###
+
+
+
+
+class MyAdminIndexView(AdminIndexView):
+
+    def is_accessible(self):
+        if current_user.is_authenticated:
+            return current_user.login == 'admin'
+        else:
+            return False
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('index'))
+
+
+admin = Admin(app, index_view=MyAdminIndexView())
+admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(BlogPost, db.session))
+admin.add_view(ModelView(Categories, db.session))
+
 
 ###__________________LOGIN MANAGER POUR Flask-Login____________________________###
 
-# @login_manager.user_loader
-# def load_user(user_id):
-#     return User.query.get(int(id))
+@login_manager.user_loader
+def load_user(id):
+     return User.query.get(int(id))
 
 @app.route('/')
 def index():
+
     return render_template('squelette.html')
 
+
 @app.route('/addpost', methods=['GET','POST'])
+@login_required
 def add_post():
     form=PostForm()
+    print("par la")
     if form.validate_on_submit() :
         post=BlogPost(title=form.title.data,
                       course=form.course.data,
                       content=form.content.data,
                       date=datetime.datetime.now())
+        print('ok')
         db.session.add(post)
         db.session.commit()
         #return redirect(url_for('index'))
         post=BlogPost.query.filter_by(title=form.title.data).first()
         return redirect(url_for('view_post',post_id=post.id_post))
     else :
+        print("Ben non")
         return render_template('add_post.html',form=form)
+
+@app.route('/update_addpost',methods=['POST'])
+def update_addpost():
+    if request.form['course'] :
+        course=request.form['course']
+
     
 @app.route('/viewpost/<int:post_id>')
 def view_post(post_id):
     post=BlogPost.query.filter_by(id_post=post_id).first()
     return render_template('one_post.html', post=post)
 
+@app.route('/login',methods=['GET','POST'])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit() :
+        user = User.query.filter_by(login=form.login.data).first()
+        if user :
+            if user.password==form.password.data :
+                login_user(user)
+                if current_user.login=="admin" :
+                    return redirect(url_for('admin.index'))
+                else :
+                    return redirect(url_for('index'))
+            flash(u'La combinaison login/mot de passe est inconnue!')
+    return render_template("login.html",form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ init_db.py
@@ -0,0 +1,61 @@
+from app import db, User, Categories
+import os
+
+if os.path.exists('static/zonensidb.sqlite3'):
+    os.remove('static/zonensidb.sqlite3')
+db.create_all()
+user = User(login="admin", password='1234')
+db.session.add(user)
+db.session.commit()
+racine = Categories(little_name='root', real_name='Root', parent=None, idg=1, idd=2)
+db.session.add(racine)
+
+db.session.commit()
+
+
+def add_category(little_name, real_name, parent=1):
+    father=Categories.query.filter_by(idg=parent).first()
+    categories = Categories.query.all()
+    for c in categories:
+        if c.idg > father.idg: c.idg += 2
+        if c.idd > father.idg: c.idd += 2
+        if c.parent !=None and c.parent> father.idg : c.parent+=2
+        db.session.commit()
+    db.session.add(
+        Categories(little_name=little_name, real_name=real_name, parent=parent, idg=parent + 1, idd=parent + 2))
+    db.session.commit()
+
+
+course = [('maths', 'Mathématiques'), ('snt', 'SNT'), ('nsi', 'NSI'), ('enssci', 'Enseignement Scientifique'),
+          ('misc', 'Miscellanées')]
+
+for ln, rn in course[::-1]:
+    add_category(ln, rn)
+
+num = Categories.query.filter_by(little_name="maths").first().idg
+cat = [('2de', 'Seconde'), ('1ereG', 'Première Générale'), ('TleG', 'Terminale Générale'),
+       ('1ereT', 'Première Technologique'),
+       ('TleT', 'Terminale Technologique'), ('mathexp', 'Maths Expertes'), ('mathcomp', 'Maths Complémentaires'),
+       ('cultmath', 'Culture Mathématique')]
+for ln, rn in cat[::-1]:
+    add_category(ln, rn, parent=num)
+
+num = Categories.query.filter_by(little_name="nsi").first().idg
+cat = [('1ereG', 'Première Générale'), ('TleG', 'Terminale Générale'), ('cultinfo', 'Culture Informatique')]
+for ln, rn in cat[::-1]:
+    add_category(ln, rn, parent=num)
+
+num = Categories.query.filter_by(little_name="misc").first().idg
+cat = [('python', 'Python'), ('web', 'Web'), ('reseaux', 'Réseaux'),('linux','Linux')]
+for ln, rn in cat[::-1]:
+    add_category(ln, rn, parent=num)
+
+
+num = Categories.query.filter_by(little_name="2de").first().idg
+cat = [('C01', 'C01'), ('C02', 'C02'), ('C03', 'C03')]
+for ln, rn in cat[::-1]:
+    add_category(ln, rn, parent=num)
+
+
+def find_children(idg):
+    return Categories.query.filter_by(parent=idg).order_by(Categories.idg).all()
